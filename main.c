23d982c7dc114a9e5356b356af4f4a47dbf4aa08
SionGriffiths <sig2@aber.ac.uk>
2013-11-21 13:22:52 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: sig2
+ *
+ * Created on 19 November 2013, 18:15
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+  return (EXIT_SUCCESS);
+}
+
