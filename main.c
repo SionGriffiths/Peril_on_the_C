2b22dc351fc740d4e83fe5b129686664743a0563
SionGriffiths <sig2@aber.ac.uk>
2013-12-05 10:00:06 +0000

    Adding new files and nav library

--- main.c
+++ main.c
@@ -10,26 +10,40 @@
 #include "rescue_assets.h"
 #include "ships.h"
 #include "mytime.h"
+
 /*
  * 
  */
 int main(int argc, char** argv) {
 
-  
+
   printf("\n\n\t\t\tPERIL ON THE C!\n\n ");
   printf("Please enter the name of a rescue assets file : ");
   char ra_filename[30];
-  scanf("%s", ra_filename);  
+  scanf("%s", ra_filename);
   make_asset_list(ra_filename);
-  
+
   printf("Please enter the name of a ships file : ");
   char ships_filename[30];
-  scanf("%s", ships_filename);  
+  scanf("%s", ships_filename);
   make_ship_list(ships_filename);
-  
+
   time_ptr start_time = get_current_time();
-  
-  printf("Please enter the name of a ships file : ");
+
+  printf("Please enter the name of a mayday file : ");
+  char mayday_filename[30];
+  scanf("%s", mayday_filename);
+  read_mayday(mayday_filename);
+
+
+
+
+
+  return (EXIT_SUCCESS);
+}
+
+/*
+ printf("Please enter the name of a ships file : ");
   char ships_filename2[30];
   scanf("%s", ships_filename2);  
   make_ship_list(ships_filename2);
@@ -40,8 +54,4 @@ int main(int argc, char** argv) {
   show_time(new_time);
   
   printf("%.f", time_diff(new_time, start_time));
-  
-  
-  return (EXIT_SUCCESS);
-}
-
+ */
--- mayday.c
+++ mayday.c
@@ -7,7 +7,46 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "mayday.h"
 
+mayday_ptr read_mayday(char * mayday_file_name) {
+
+  FILE * mayday_file;
+  int read_status;
+  mayday * mayday_in;
+
+  mayday_file = fopen(mayday_file_name, "r");
+
+  if (mayday_file != NULL) {
+
+    mayday_in = calloc(1, sizeof (mayday));
+    
+    read_status = fscanf(mayday_file, "%d%d%d%d%d%d%s%d%d", 
+            &mayday_in->day,
+            &mayday_in->month,
+            &mayday_in->year,
+            &mayday_in->hours,
+            &mayday_in->minutes,
+            &mayday_in->seconds,
+            mayday_in->ais_id,
+            &mayday_in->boat_minutes,
+            &mayday_in->heli_minutes);
+    
+    if(read_status == EOF){
+      return NULL;
+    }
+    else if(read_status != 9){
+      printf("Mayday file looks incorrect or corrupt \n");
+    }
+    else{
+      printf("Mayday read OK");
+      return mayday_in;
+    }
+
+  }
+
+  return NULL;
+}
 
 
 
